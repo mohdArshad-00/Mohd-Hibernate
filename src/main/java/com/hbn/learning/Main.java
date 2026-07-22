f93aff3015d1049c21f254fd730fbcb2dab0f559
Mohd Arshad <mohdarshadansari83@gmail.com>
2026-07-22 10:29:11 +0530

    Initial commit

---
+++ src/main/java/com/hbn/learning/Main.java
@@ -0,0 +1,27 @@
+package com.hbn.learning;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.hbn.learning.entity.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee("Muskan", "Female", 58000);
+		
+		
+		Session session = HibernateConfig.getSessionFactory().openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		
+		session.persist(emp);
+		transaction.commit();
+
+		
+	}
+
+}
