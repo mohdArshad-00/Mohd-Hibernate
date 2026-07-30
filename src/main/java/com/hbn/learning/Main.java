02a7ed9ca8ab7300e5e98ae6ca73fd5486d960ae
Mohd Arshad <mohdarshadansari83@gmail.com>
2026-07-30 12:15:50 +0530

    update

--- src/main/java/com/hbn/learning/Main.java
+++ src/main/java/com/hbn/learning/Main.java
@@ -1,9 +1,13 @@
 package com.hbn.learning;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.MutationQuery;
+import org.hibernate.query.Query;
 
 import com.hbn.learning.entity.Employee;
 
@@ -11,14 +15,46 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Employee emp = new Employee("Mohd", "male", 58000);
+		Employee emp = new Employee("Sayud", "male", 62000);
 		
 		
 		Session session = HibernateConfig.getSessionFactory().openSession();
 		Transaction transaction = session.beginTransaction();
 		
 		
-		transaction.commit();
+//		Query query = session.createQuery("from employee", Employee.class);
+//		List list = query.list();
+//		System.out.println(list);
+		
+		
+//		Query query = session.createQuery("from employee", Employee.class);
+//		query.setFirstResult(2);
+//		query.setMaxResults(4);
+//		List list = query.list();
+//		System.out.println(list);
+		
+//		Query query = session.createQuery("update employee set name = :n, salary = :s where is = :i", Employee.class);
+//		
+//		query.setParameter("n", "Afzal");
+//		query.setParameter("s", "154321");
+//		query.setParameter("i", "2");
+//		query.executeUpdate();
+		
+		
+//		MutationQuery query = session.createMutationQuery("delete from employee where id = :i");
+//		query.setParameter("i", "6");
+//		query.executeUpdate();
+//		transaction.commit();
+		
+		
+
+
+		
+		
+		Query query1 = session.createQuery("SELECT SUM(salary) from employee");
+//		List list = query1.list();
+		System.out.println(query1.list());
+
 
 		
 	}
